059500a6a0eb516b7461a9586cb3fb42fed9cf1e
Maciej Ziaja <maciejzjg@gmail.com>
2021-04-21 16:26:12 +0200

    Draft gan more

--- cnn_res_degrader/gan.py
+++ cnn_res_degrader/gan.py
@@ -7,66 +7,45 @@ from pathlib import Path
 from typing import Any, Callable, Dict, List, Tuple
 
 import tensorflow as tf
+import numpy as np
 from tensorflow import keras
 from tensorflow.keras.callbacks import (
-        EarlyStopping,
-        ModelCheckpoint,
-        TensorBoard)
-
+    EarlyStopping,
+    ModelCheckpoint,
+    TensorBoard)
 from cnn_res_degrader.data_loading import (
-        Dataset,
-        Subset,
-        InterpolationMode,
-        ProbaDataGenerator,
-        ProbaDirectoryScanner,
-        ProbaImagePreprocessor,
-        ProbaHistEqualizer,
-        ProbaHrToLrResizer)
+    Dataset,
+    Subset,
+    InterpolationMode,
+    ProbaDataGenerator,
+    ProbaDirectoryScanner,
+    ProbaImagePreprocessor,
+    ProbaHistEqualizer,
+    ProbaHrToLrResizer)
 from cnn_res_degrader.metrics import make_ssim_metric
 from cnn_res_degrader.models import make_model, Models
-
-def make_training_data(
-            dataset: Dataset,
-            input_shape: Tuple[int, int, int],
-            validation_split: float,
-            preprocessor_params: Dict[str, Any],
-            limit_per_scene: int) -> Tuple[ProbaDataGenerator, ProbaDataGenerator]:
-
-    dir_scanner = ProbaDirectoryScanner(
-            Path('data/proba-v11_shifted'),
-            dataset=dataset,
-            subset=Subset.TRAIN,
-            splits={'train': validation_split, 'val': 1.0 - validation_split},
-            limit_per_scene=limit_per_scene)
-    preprocessor = make_preprocessor(preprocessor_params)
-
-    train_ds = ProbaDataGenerator(
-            dir_scanner.get_split('train'),
-            hr_shape=input_shape,
-            preprocessor=preprocessor)
-
-    val_ds = ProbaDataGenerator(
-            dir_scanner.get_split('val'),
-            hr_shape=input_shape,
-            preprocessor=preprocessor)
-
-    return train_ds, val_ds
-
+from cnn_res_degrader.train import make_training_data
+from cnn_res_degrader.test import make_test_data
+from matplotlib import pyplot as plt
 
 # Create the discriminator
 discriminator = keras.Sequential(
     [
         keras.Input(shape=(126, 126, 1)),
-        layers.Conv2D(64, (3, 3), strides=(2, 2), padding="same"),
-        layers.LeakyReLU(alpha=0.2),
-        layers.Conv2D(64, (3, 3), strides=(2, 2), padding="same"),
-        layers.LeakyReLU(alpha=0.2),
-        layers.GlobalMaxPooling2D(),
-        layers.Dense(1),
+        layers.Conv2D(64, (3, 3), strides=(2, 2),
+                      padding="same"),
+        layers.LeakyReLU(),
+        layers.MaxPool2D(),
+        layers.Conv2D(64, (3, 3), strides=(2, 2),
+                      padding="same"),
+        layers.MaxPool2D(),
+        layers.LeakyReLU(),
+        layers.Flatten(),
+        layers.Dropout(rate=0.5),
+        layers.Dense(1, activation='sigmoid'),
     ],
     name="discriminator",
 )
-
 
 
 # Create the generator
@@ -74,55 +53,56 @@ generator = keras.Sequential(
     [
         keras.Input(shape=(378, 378, 1)),
         layers.Conv2D(64, kernel_size=3, padding="same", activation='relu'),
-        layers.Conv2D(64, kernel_size=3, strides=3, padding="same", activation='relu'),
+        layers.Conv2D(64, kernel_size=3, strides=3,
+                      padding="same", activation='relu'),
         layers.Conv2D(1, kernel_size=3, padding="same", activation='sigmoid'),
     ],
     name="generator",
 )
+
 
 class GAN(keras.Model):
     def __init__(self, discriminator, generator):
         super(GAN, self).__init__()
         self.discriminator = discriminator
         self.generator = generator
-        
+
     def compile(self, d_optimizer, g_optimizer, loss_fn):
         super(GAN, self).compile()
         self.d_optimizer = d_optimizer
         self.g_optimizer = g_optimizer
         self.loss_fn = loss_fn
-        
+
     def train_step(self, data):
         x, y, y_mask = data
-        
+
         # Sample random points in the latent space
         batch_size = tf.shape(x)[0]
-        
         y_fake = self.generator(x)
-        
+
         # Combine them with real images
         combined_images = tf.concat([y_fake, y], axis=0)
-        
+
         # Assemble labels discriminating real from fake images
         labels = tf.concat(
-            [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
+            [tf.zeros((batch_size, 1)), tf.ones((batch_size, 1))], axis=0
         )
         # Add random noise to the labels - important trick!
         labels += 0.05 * tf.random.uniform(tf.shape(labels))
-        
+
         # Train the discriminator
         with tf.GradientTape() as tape:
             y_pred = self.discriminator(combined_images)
             d_loss = self.loss_fn(labels, y_pred)
-        
+
         grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
         self.d_optimizer.apply_gradients(
             zip(grads, self.discriminator.trainable_weights)
         )
-        
+
         # Assemble labels that say "all real images"
-        misleading_labels = tf.zeros((batch_size, 1))
-        
+        misleading_labels = tf.ones((batch_size, 1))
+
         # Train the generator (note that we should *not* update the weights
         # of the discriminator)!
         with tf.GradientTape() as tape:
@@ -130,63 +110,77 @@ class GAN(keras.Model):
             g_loss = self.loss_fn(misleading_labels, y_pred)
 
         grads = tape.gradient(g_loss, self.generator.trainable_weights)
-        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
+        self.g_optimizer.apply_gradients(
+            zip(grads, self.generator.trainable_weights))
         return {"d_loss": d_loss, "g_loss": g_loss}
-    
-    
+
     def call(self, x):
-        x = self.discriminator(x)
+        x = self.generator(x)
         return x
-    
-    
+
+
 def make_preprocessor(prep_params: Dict[str, Any]) -> ProbaImagePreprocessor:
-        transformations = []
-    
-        if prep_params['equalize_hist']:
-                transformations.append(ProbaHistEqualizer())
-            
-        if prep_params['artificial_lr']:
-                transformations.append(ProbaHrToLrResizer(
-                        prep_params['interpolation_mode']))
-            
-        return ProbaImagePreprocessor(*transformations)
+    transformations = []
+
+    if prep_params['equalize_hist']:
+        transformations.append(ProbaHistEqualizer())
+
+    if prep_params['artificial_lr']:
+        transformations.append(ProbaHrToLrResizer(
+            prep_params['interpolation_mode']))
+
+    return ProbaImagePreprocessor(*transformations)
 
 
 def make_params(params_path: Path) -> Dict[str, Any]:
-        with open(params_path) as params_file:
-                params = yaml.load(params_file, Loader=yaml.FullLoader)
-            
-        params['model'] = Models[params['model']]
-        params['load']['dataset'] = Dataset[params['load']['dataset']]
-        prep = params['load']['preprocess']
-        prep['interpolation_mode'] = InterpolationMode[prep['interpolation_mode']]
+    with open(params_path) as params_file:
+        params = yaml.load(params_file, Loader=yaml.FullLoader)
 
-    
-        return params
+    params['model'] = Models[params['model']]
+    params['load']['dataset'] = Dataset[params['load']['dataset']]
+    prep = params['load']['preprocess']
+    prep['interpolation_mode'] = InterpolationMode[prep['interpolation_mode']]
+
+    return params
 
 
 def main():
-        params = make_params(Path('params.yaml'))
-    
-        train_ds, val_ds = make_training_data(
-                params['load']['dataset'],
-                params['load']['input_shape'],
-                params['load']['validation_split'],
-                params['load']['preprocess'],
-                params['load']['limit_per_scene'])
-    
-        gan = GAN(discriminator=discriminator, generator=generator)
-        gan.compile(
-            d_optimizer=keras.optimizers.Adam(learning_rate=0.0003),
-            g_optimizer=keras.optimizers.Adam(learning_rate=0.0003),
-            loss_fn=keras.losses.BinaryCrossentropy(from_logits=True),
-        )
-    
-        # To limit the execution time, we only train on 100 batches. You can train on
-        # the entire dataset. You will need about 20 epochs to get nice results.
-        gan.fit(train_ds, epochs=1)
-    
-    
+    params = make_params(Path('params.yaml'))
+
+    train_ds, val_ds = make_training_data(
+        params['load']['dataset'],
+        params['load']['input_shape'],
+        params['load']['batch_size'],
+        params['load']['validation_split'],
+        params['load']['preprocess'],
+        params['load']['limit_per_scene'])
+
+    gan = GAN(discriminator=discriminator, generator=generator)
+    gan.compile(
+        d_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+        g_optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+        loss_fn=keras.losses.BinaryCrossentropy(),
+    )
+
+    gan.fit(train_ds, epochs=50)
+    test_ds = make_test_data(
+        params['load']['dataset'],
+        params['load']['input_shape'],
+        params['load']['batch_size'])
+    # To limit the execution time, we only train on 100 batches. You can train on
+    # the entire dataset. You will need about 20 epochs to get nice results.
+    pred = gan(np.expand_dims(test_ds[29][0][16], axis=0))
+    plt.figure()
+    plt.title('pred inv')
+    plt.imshow(1 - pred[0])
+    plt.figure()
+    plt.title('inv')
+    plt.imshow(pred[0])
+    plt.figure()
+    plt.title('lr')
+    plt.imshow(test_ds[29][1][16][1:-1,1:-1,:])
+    plt.show()
+
+
 if __name__ == '__main__':
-        main()
-    
+    main()
